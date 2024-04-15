0faf049e1ecbc03cf0a177d87c90b515bfe9c677
gmmarsh <gmmarsh@gmail.com>
2024-04-14 20:05:58 -0400

    changed code in logic.js

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,32 +1,36 @@
-// fetch GeoJSON data
-d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson", function(data) {
-  createFeatures(data.features);
-});
-
 // initialize the map
-var map = L.map('map').setView([37.8, -96], 4);
+let map = L.map('map').setView([37.8, -96], 4);
 
 // tile layer that will be the background of the map
-var tileLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+let tileLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
 }).addTo(map)
 
-// earthquake marker
-function createFeatures(earthquakeData) {
-    earthquakeData.forEach((feature) => {
-        var lat = feature.geometry.coordinates[1];
-        var lon = feature.geometry.coordinates[0];
-        var mag = feature.properties.mag;
-        var place = feature.properties.place;
-        var time = new Date(feature.properties.time)    
+// pull the data and print it to the console
+let url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+d3.json(url).then(data => {
+    console.log(data)
 
-        var circle = L.circle([lat, lon], {
-            color: "black",
-            weight: 1,
-            fillColor: getColor(mag),
-            fillOpacity: 0.8,
-            radius: mag * 15000
-        }).bindPopup(`<h3>${place}</h3><hr><p>${new Date(time)}</p>`).addTo(map);
-    });
+    // for loop to create the markers
+    let earthquakes = data.features
+    for (let i = 0; i < earthquakes.length; i++) {
+        let earthquake = earthquakes[i]
+        let lat = earthquake.geometry.coordinates[1]
+        let lon = earthquake.geometry.coordinates[0]
+        let mag = earthquake.properties.mag
+        let place = earthquake.properties.place
+        let time = earthquake.properties.time
+
+    // create a circle marker
+        let circle = L.circle([lat, lon], {
+            color: 'black',
+            fillColor: 'red',
+            fillOpacity: 0.5,
+            radius: mag * 20000
+        }).addTo(map)
+
+        // bind a popup to the circle marker
+        circle.bindPopup(`<h1>${place}</h1><hr><h3>Magnitude: ${mag}</h3><h3>Time: ${new Date(time)}</h3>`)
 }
+})
